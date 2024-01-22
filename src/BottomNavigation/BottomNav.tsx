01e3310d711e769f7c8516329a107480dc256a45
samrat neupane <samrat.neupane17013@gmail.com>
2024-01-22 23:21:46 +0545

    Nav completed

--- src/BottomNavigation/BottomNav.tsx
+++ src/BottomNavigation/BottomNav.tsx
@@ -6,24 +6,91 @@ import Home from '../components/Home';
 import Trending from '../components/Trending';
 import Blog from '../components/Blog';
 import About from '../components/About';
-
-const Tab = createBottomTabNavigator();
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Fire from 'react-native-vector-icons/Fontisto';
+import Blogger from 'react-native-vector-icons/FontAwesome6';
+import User from 'react-native-vector-icons/FontAwesome';
+import ScanedInfo from '../components/ScanedInfo';
+const Bottom = createBottomTabNavigator();
 
 export default function BottomNav() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
+      <Bottom.Navigator
         initialRouteName="Home"
         screenOptions={{
+          tabBarShowLabel: false,
           tabBarStyle: {
             backgroundColor: '#101A56',
+            backfaceVisibility: 'hidden',
+            borderTopRightRadius: 50,
+            borderTopLeftRadius: 50,
+            borderTopColor: '#EDF2EF',
           },
         }}>
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Trending" component={Trending} />
-        <Tab.Screen name="Blog" component={Blog} />
-        <Tab.Screen name="About" component={About} />
-      </Tab.Navigator>
+        <Bottom.Screen
+          name="Home"
+          component={Home}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({color}) => (
+              <AntDesign name="home" size={24} color="#D9D9D9" />
+            ),
+          }}
+        />
+        <Bottom.Screen
+          name="Inf"
+          component={ScanedInfo}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({color}) => (
+              <User name="user-circle-o" size={24} color="#D9D9D9" />
+            ),
+          }}
+        />
+        {/* --UNCOMMENT THIS AFTER SCANNEDINFO COMPLETED
+         <Bottom.Screen
+          name="Trending"
+          component={Trending}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({color}) => (
+              <Fire name="fire" size={24} color="#D9D9D9" />
+            ),
+          }}
+        /> */}
+
+        <Bottom.Screen
+          name="Scan"
+          component={Home}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({color}) => (
+              <AntDesign name="qrcode" size={44} color="#D9D9D9" />
+            ),
+          }}
+        />
+        <Bottom.Screen
+          name="Blog"
+          component={Blog}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({color}) => (
+              <Blogger name="blog" size={24} color="#D9D9D9" />
+            ),
+          }}
+        />
+        <Bottom.Screen
+          name="About"
+          component={About}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({color}) => (
+              <User name="user-circle-o" size={24} color="#D9D9D9" />
+            ),
+          }}
+        />
+      </Bottom.Navigator>
     </NavigationContainer>
   );
 }
---
+++ src/components/ScanedInfo.tsx
@@ -0,0 +1,12 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+export default function ScanedInfo() {
+  return (
+    <View>
+      <Text>ScanedInfo</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
